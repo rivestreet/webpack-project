cbbffd26eccf3b28e77dd6e18033edcfe2068ab7
riverstreet <1787552935@qq.com>
2022-06-17 22:50:54 +0800

    完成了vue功能！！

--- src/main.js
+++ src/main.js
@@ -1,26 +1,33 @@
-import {banner} from './banner.js'
-import {tab} from './tab.js'
-banner()
-tab()
-import './styles/index.css'
-import './styles/index.less'
-import './assets/fonts/iconfont.css'
+import { banner } from "./banner.js";
+import { tab } from "./tab.js";
+banner();
+tab();
+import "./styles/index.css";
+import "./styles/index.less";
+import "./assets/fonts/iconfont.css";
 //引入图片
-import gifSrc from './assets/1.gif'
-import pngSrc from './assets/logo_small.png'
+import gifSrc from "./assets/1.gif";
+import pngSrc from "./assets/logo_small.png";
 
 //创建图片节点
-const gif = document.createElement('img')
-const png = document.createElement('img')
+const gif = document.createElement("img");
+const png = document.createElement("img");
 
 //给src赋值
-gif.src = gifSrc
-png.src = pngSrc
+gif.src = gifSrc;
+png.src = pngSrc;
 
 //插入节点
-document.body.appendChild(gif)
-document.body.appendChild(png)
- const fn = () => {
-     console.log('你好');
- } 
- console.log(fn);
+document.body.appendChild(gif);
+document.body.appendChild(png);
+const fn = () => {
+  console.log("你好");
+};
+console.log(fn);
+
+import App from "./App.vue"; // 根vue文件
+import Vue from "vue"; // 引入vue.js对象
+
+new Vue({
+  render: (h) => h(App), // 渲染函数, 渲染App组件里的标签
+}).$mount("#app"); // 把vue文件的标签结构 -> 挂载到id为app的标签里
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 // 引入自动生成 html 的插件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 module.exports = {
   mode: "development",
   entry: "./src/main.js", // 入口
@@ -14,49 +15,54 @@ module.exports = {
       template: "./public/index.html",
       filename: "index.html",
     }),
+    new VueLoaderPlugin(),
   ],
   devServer: {
     port: 65535, // 端口号
     open: true,
   },
   module: {
-    rules: [{
-      test: /\.css$/i, // 匹配所有的css文件
-      // use数组里从右向左运行
-      // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
-      // 再用 style-loader 将样式, 把css插入到dom中
-      use: [ "style-loader", "css-loader"]
-    
-    },
-    {
-      test: /\.less$/,
-    	// 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
-        use: [ "style-loader", "css-loader", 'less-loader']
-    },
-    {
-      test:/\.(png|jpg|gif)$/i,
-      type :'asset',
-      generator: {
-        filename:"images/[hash:6][ext]",
-      }
-    },
-    {
-      test:/.(eot|svg|ttf|woff|woff2)$/i,
-      type:"asset/resource",
-      generator:{
-        filename:"images/[hash:6][ext]",
-      }
-    },
-    {
-      test:/\.js$/,
-      exclude: /(node_modules)/,
+    rules: [
+      {
+        test: /\.css$/i, // 匹配所有的css文件
+        // use数组里从右向左运行
+        // 先用 css-loader 让webpack能够识别 css 文件的内容并打包
+        // 再用 style-loader 将样式, 把css插入到dom中
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        // 使用less-loader, 让webpack处理less文件, 内置还会用less翻译less代码成css内容
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: "asset",
+        generator: {
+          filename: "images/[hash:6][ext]",
+        },
+      },
+      {
+        test: /.(eot|svg|ttf|woff|woff2)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash:6][ext]",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
         use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'] // 预设:转码规则(用bable开发环境本来预设的)
-            }
-          }
-    }
-  ],
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"], // 预设:转码规则(用bable开发环境本来预设的)
+          },
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
   },
 };
